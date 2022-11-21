3d2d024f77f6fec48bcad9ad6f6b9b02a09a19a5
Sneekee-stache <kyle.prudhomme@proton.me>
2022-11-21 12:51:34 +0100

    orientation vent variable + animation + fermer

--- Assets/player.cs
+++ Assets/player.cs
@@ -6,8 +6,18 @@ using DG.Tweening;
 public class player : MonoBehaviour
 {
     bool Collision=false;
+    bool fermer=false;
     Rigidbody rb;
     Animator animatorPlayer;
+    [SerializeField] float drag=7;
+    [SerializeField] float dragFermer=0;
+    [SerializeField] float ImpulseOrientationPlayer;
+    [SerializeField] float forceOrientationAnimation;
+    [SerializeField] Vector3 OrientationVent=Vector3.zero;
+
+
+    Vector3 orientationModif;
+    Vector3 orientationAnim;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,14 +29,28 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.AddForce(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"),ForceMode.Impulse);
+        orientationModif=OrientationVent+ new Vector3(Input.GetAxis("Horizontal")*ImpulseOrientationPlayer,0,Input.GetAxis("Vertical")*ImpulseOrientationPlayer);
+        orientationAnim=OrientationVent+ new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+        if(Input.GetButtonDown("Jump")){
+            fermer=true;
+        }else{
+            fermer=false;
+        }
+
+        if(fermer){
+            rb.drag=dragFermer;
+        }else{
+            rb.drag=drag;
+        }
+
+        rb.AddForce(orientationModif,ForceMode.Impulse);
         
         Debug.Log(Collision);
         if(Collision){
             rb.freezeRotation=false;
         }else{
             rb.freezeRotation=true;
-            transform.DORotateQuaternion(Quaternion.Euler(60*Input.GetAxis("Vertical"),0,-60*Input.GetAxis("Horizontal")),1);
+            transform.DORotateQuaternion(Quaternion.Euler(orientationAnim.z*forceOrientationAnimation,0,-orientationAnim.x*forceOrientationAnimation),1);
         }
         
     }

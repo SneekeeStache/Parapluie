db0349a1a0a2dd1a0c521acbf90ccbacf83ad577
SneekeeStache <kyle.prudhomme@proton.me>
2023-01-02 09:34:16 +0100

    fix thomas

--- Assets/test PROJET ANNUEL/CountElement.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CountElement : MonoBehaviour
-{
-    //variable pour numéroter les selections de la barre d'outil du player
-    public float Count;
-}
--- Assets/test PROJET ANNUEL/State Machine/TemplateState.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public abstract class TemplateState
-{
-    public abstract void StartState(StateMachineMob1 Mob1);
-    public abstract void UpdateState(StateMachineMob1 Mob1);
-    
-}

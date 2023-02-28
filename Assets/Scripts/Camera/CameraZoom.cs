64a5af12d31969c1cbdd997a31e76c36456cb84b
SneekeeStache <kyle.prudhomme@proton.me>
2023-02-28 18:43:03 +0100

    camera wall test

--- Assets/Scripts/Camera/CameraZoom.cs
+++ Assets/Scripts/Camera/CameraZoom.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +8,21 @@ public class CameraZoom : MonoBehaviour
     public player parapluie;
     public float minC, maxC;
     public float Cvalue;
+
+    public float smooth=10;
+    Vector3 dollyDir;
+    public Vector3 dollyDirAdjusted;
+    public float distance;
+    public LayerMask ignoreLayer;
+
+    public float distanceCenterCamera=50;
+    public float vitesseZoom;
+
+    private void Awake() {
+        dollyDir = transform.transform.localPosition.normalized;
+        distance=transform.localPosition.magnitude;
+    }
+    
 
     [Header("Cinematique de fin")]
     public float speedCam;
@@ -24,6 +38,31 @@ public class CameraZoom : MonoBehaviour
 
     void Update()
     {
+        distanceCenterCamera=Mathf.Clamp(distanceCenterCamera,minC,maxC);
+
+        if (Input.mouseScrollDelta.y > 0f)
+        {
+            
+            distanceCenterCamera+=1*vitesseZoom*Time.deltaTime;
+        }
+        if (Input.mouseScrollDelta.y < 0f)
+        {
+            distanceCenterCamera-=1*vitesseZoom*Time.deltaTime;
+        }
+        Vector3 desiredCameraPos= transform.parent.TransformPoint(dollyDir * distanceCenterCamera);
+        RaycastHit hit;
+        if(Physics.Linecast(transform.parent.position,desiredCameraPos,out hit,ignoreLayer)){
+            print(hit.collider.gameObject.name);
+            distance=Mathf.Clamp((hit.distance * 0.87f),minC,distanceCenterCamera);
+
+        }else{
+            distance=distanceCenterCamera;
+        }
+
+        transform.localPosition = Vector3.Lerp (transform.localPosition, dollyDir * distance, Time.deltaTime * smooth);
+
+        Debug.DrawLine(transform.parent.position,desiredCameraPos,Color.red);
+        
         if (parapluie.end)
         {
             if (timeCam >= 0)
@@ -34,16 +73,5 @@ public class CameraZoom : MonoBehaviour
                 timeCam -= Time.deltaTime;
             }
         }
-        /*if (Input.mouseScrollDelta.y > 0f && Cvalue<maxC)
-        {
-            
-            transform.position += transform.forward;
-            Cvalue += 0.5f;
-        }
-        if (Input.mouseScrollDelta.y < 0f && Cvalue>minC)
-        {
-            transform.position += -transform.forward;
-            Cvalue -= 0.5f;
-        }*/
     }
 }
